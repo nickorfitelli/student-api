ca6416648f5eb1b9b3cd8adfe8bfff77bac065ad
nickorfitelli <nicholas.orfitelli.1@us.af.mil>
2020-06-22 16:59:34 -0400

    new search feature

--- app.js
+++ app.js
@@ -8,23 +8,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 const studentData = JSON.parse(fs.readFileSync("students.JSON"))
 
-app.get('/students?search', (req, res) => res.send(students))
+app.get('/students', (req, res) => {
+    let student = req.query.search
+    
+    if(student){res.send(studentData.filter(x => x['name'].includes(student)))}
+    res.send(studentData)
+})
 
 app.get('/:id', (req, res) => {
     /* GET a user by their id */
      res.send(users[req.params.id-1]
 )})
 
-app.post('/', (req, res) => {
-    /* POST user data using the request body */
-    res.send('POST user data using req body')
-})
-
-app.get('/name=?', (req, res) => {
-    /* GET a user by their name */
-    res.send("query a name")
-    res.send(users[req.query.name]
-)})
 
 const port = 3000
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
